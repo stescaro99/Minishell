591fb0c14613db78c723f9a4f77b0b31b50a15a8
stescaro99 <150029705+stescaro99@users.noreply.github.com>
2024-04-24 15:04:18 +0200

    Add files via upload

---
+++ src/minishell.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stescaro <stescaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 13:40:16 by stescaro          #+#    #+#             */
+/*   Updated: 2024/04/24 13:55:05 by stescaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static	void	handle_par_logic(t_line **l, t_line **par)
+{
+	int		e;
+
+	e = 0;
+	if ((*par)->exit == 10)
+	{
+		(*l)->exit = 10;
+		return ;
+	}
+	if ((*par)->exit == 0 && ++e)
+		(*par)->exit = 1;
+	else if (--e)
+		(*par)->exit = -1;
+	handle_logic(par);
+	if (!(*par))
+	{
+		(*l)->exit = e;
+		handle_logic(l);
+	}
+}
+
+static	void	handle_miniparentesis(t_data *data, t_line **line, t_line **par)
+{
+	t_line	*temp;
+	int		res;
+	int		pid;
+
+	if (!par || !*par)
+		return ;
+	temp = *par;
+	res = -1;
+	pid = fork();
+	if (pid == 0)
+	{
+		res = minishell(data, temp, 0, 10);
+		exit(res);
+	}
+	else if (pid > 0)
+		waitpid(pid, &res, 0);
+	(*par)->exit = WEXITSTATUS(res);
+	handle_par_logic(line, par);
+}
+
+static	int	shell_cmds(t_data **data, t_line **tmp)
+{
+	if ((*tmp)->type == 1)
+		return (pipe_shell(*data, tmp));
+	else if ((*tmp)->type == 2)
+		return (echo_shell(tmp));
+	else if ((*tmp)->type == 3)
+		return (cd_shell(*data, tmp));
+	else if ((*tmp)->type == 4)
+		return (pwd_shell(tmp));
+	else if ((*tmp)->type == 5)
+		return (export_shell(&((*data)->env), tmp));
+	else if ((*tmp)->type == 6)
+		return (unset_shell(&((*data)->env), tmp));
+	else if ((*tmp)->type == 7)
+		return (env_shell((*data)->env, tmp));
+	else if ((*tmp)->type == 8)
+		return (memory_shell(*data, tmp));
+	else
+		return (exec_shell(*data, tmp));
+}
+
+int	minishell(t_data *data, t_line *tmp, int ret, int ext)
+{
+	while (tmp)
+	{
+		if (tmp->type == -1 || tmp->exit > 9)
+			return (ext);
+		else if (tmp->type < 9 && tmp->type > -1)
+			ret += shell_cmds(&data, &tmp);
+		else if (tmp->type == 9)
+		{
+			handle_miniparentesis(data, &tmp, &(tmp->parentesis));
+			tmp = tmp->next;
+		}
+	}
+	return (ret);
+}
